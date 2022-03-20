7718207fcb7c28a3401d9ed3463f8450c483afa1
Tom <aerlin86@gmail.com>
2022-03-20 19:48:02 +0100

    PRE-ALPHA: added pandas Excel read and data extraction

--- Mechanics.py
+++ Mechanics.py
@@ -1,12 +1,41 @@
 # ALL CODE SOURCE FOR MECHANICS USED IN THIS PROJECT
 
-def loadfile():
-    pass
+import os, shutil, tkinter, tkinter.filedialog, re
+
+import pandas
+from fpdf import FPDF
 
 
-def save_to():
-    pass
+directory = None
 
 
-def generate():
+def loadfile() -> list:
+    """Loads Excel file and put data into list"""
+    # CHOOSING FILE
+    file = tkinter.filedialog.askopenfilename(initialdir="/", title='Proszę wybierz plik z danymi prelegentów')
+
+    # READ AND EXTRACT DATA FROM EXCEL FILE
+    db = pandas.read_excel(file, sheet_name='Arkusz1')
+    all_data_list = db.values.tolist()
+    for element in all_data_list:
+        x = type(element[0])
+        if x == float:
+            degree = ""
+        else:
+            degree = element[0]
+        name = (element[1] + " " + element[2])
+        title = (element[3])
+        # PDF FILE
+
+
+
+
+def save_to() -> dir:
+    """Choose right dir to save all PDFs"""
+    global directory
+    directory = tkinter.filedialog.askdirectory(initialdir="/", title='Proszę wybierz folder do zapisu certyfikatów')
+    return directory
+
+
+def generate() -> None:
     pass
